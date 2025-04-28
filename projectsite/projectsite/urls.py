276343a6343fff9db57fee26f12b77ca2f6523dc
Relova-John <170879264+Relova-John@users.noreply.github.com>
2025-04-28 21:59:31 +0800

    dynamic line chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth
 from fire import views
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', views.PieCountbySeverity, name='chart'),
+    path('lineChart/', views.LineCountbyMonth, name='lineChart'),
     path('station', views.map_station, name='map-station'),
     path('incident', views.map_incident, name='map-incident'),
 ]
