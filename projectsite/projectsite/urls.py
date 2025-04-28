7f1217fb7c4f767a6ee23f8981bed93748a5be03
Relova-John <170879264+Relova-John@users.noreply.github.com>
2025-04-29 01:21:51 +0800

    dynamic multibar chart + spacing control + characterization control

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
@@ -9,8 +9,9 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('chart/', views.PieCountbySeverity, name='chart'),
-    path('lineChart/', views.LineCountbyMonth, name='lineChart'),
-    path('multilineChart/', views.MultilineIncidentTop3Country, name='multilineChart'),
+    path('lineChart/', views.LineCountbyMonth, name='chart'),
+    path('multilineChart/', views.MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', views.multipleBarbySeverity, name='chart'),
     path('station', views.map_station, name='map-station'),
     path('incident', views.map_incident, name='map-incident'),
 ]
